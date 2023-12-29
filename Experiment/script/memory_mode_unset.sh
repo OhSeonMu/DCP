a7efd4d37472d43077d439d237775185cdc3f4bd
OhSeonMu <tjsan0122@gmail.com>
2023-12-29 12:00:21 +0900

    last

--- Experiment/script/memory_mode_unset.sh
+++ Experiment/script/memory_mode_unset.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 sudo ipmctl delete -goal
+# No use
 sudo ipmctl create -goal Reserved=100
+# Memory Mode
+# sudo ipmctl create -goal MemoryMode=100
+# App direct Mode
+# sudo ipmctl create -goal PersistentMemoryType=AppDirect
+# Mixed Mode
+# sudo ipmctl create -goal MemoryMode=5 PersistentMemoryType=AppDirect
 
 new_line_1='GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux 5.15.0-89-generic"'
 new_line_2='# GRUB_DEFAULT="Advanced options for Ubuntu>Ubuntu, with Linux 6.1.64"'
